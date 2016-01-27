ed717163440b186453fd4e903be08cff2be36cd5
koluku <korufiru@gmail.com>
2016-01-27 18:19:01 +0900

    その他細かい変更
    開発環境を整えるため調整

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,46 @@
+var browserSync = require('browser-sync').create();
 var gulp        = require('gulp');
+var jade        = require('gulp-jade');
 var notify      = require('gulp-notify');
 var plumber     = require('gulp-plumber');
 var pleeease    = require('gulp-pleeease');
 var sass        = require('gulp-sass');
 
 paths = {
-  root: ['./'],
-  css : ['./*.css'],
-  scss: ['./scss/*.scss']
-}
+  jade: ['./jade/'],
+  scss: ['./scss/']
+};
 
+gulp.task('browser-sync', function() {
+   browserSync.init({
+     server: {
+       baseDir: './'
+     }
+   });
+});
+gulp.task('jade', function() {
+  gulp.src(paths.jade + '**/*.jade')
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+});
 gulp.task('sass', function() {
-  gulp.src(paths.scss)
+  gulp.src(paths.scss + '**/*.scss')
     .pipe(plumber({
       errorHandler: notify.onError('Error: <%= error.message %>')
     }))
     .pipe(sass())
     .pipe(pleeease())
-    .pipe(gulp.dest(paths.root));
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch([paths.scss], ['sass']);
+gulp.task('default', ['jade','sass','browser-sync'], function() {
+  gulp.watch([paths.jade + '**/*.jade'], ['jade']);
+  gulp.watch([paths.scss + '**/*.scss'], ['sass']);
 });
